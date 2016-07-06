17726b7689a05f7462c8d1ff4914fc9811168cff
Walt Yeh <yexin5120209375@sjtu.edu.cn>
2016-07-06 14:03:57 +0800

    horizontal 2D

--- src/pos_ctrl.cpp
+++ src/pos_ctrl.cpp
@@ -43,24 +43,24 @@ struct control
 	bool flag_arrived;
 	int mode;
 	bool pos_halt[3];
-	float pos_sp[3];
+	Vector3f pos_sp;
 	bool enabled;
 };
 
 struct output
 {
-	float vel_sp[3];
+	Vector3f vel_sp;
 };
 
 raw_state raw_state;
 image_state img_state;
-struct control ctrl = {false, 0,{true,true,true},{0,0,0},false};
-struct output output = {{0,0,0}};
+control ctrl;// = {false, 0,{true,true,true},{0,0,0},false};
+output output;// = {{0,0,0}};
 
 void ctrl_sp_callback(const flight_strategy::ctrl msg)
 {
 	for(int i=0;i<3;i++){
-		ctrl.pos_sp[i] = msg.pos_sp[i];
+		ctrl.pos_sp(i) = msg.pos_sp[i];
 		if(msg.pos_halt[i])
 			ctrl.pos_halt[i] = true;
 		else
@@ -102,7 +102,7 @@ void drone_info_Callback(const image_process::drone_info msg)
 
 }
 
-bool inaccurate_control_1D(float pos_sp, float pos, float &vel_sp)
+bool inaccurate_control_1D(float pos_sp, float pos, float *vel_sp)
 {
 	float speed = 0.1;
 	bool is_arrived;
@@ -110,7 +110,7 @@ bool inaccurate_control_1D(float pos_sp, float pos, float &vel_sp)
 	float dist = fabs(err);
 	if(dist > 0.3){
 		float direction = err / dist;
-		vel_sp = direction * speed;
+		*vel_sp = direction * speed;
 		is_arrived = false;
 	}
 	else{
@@ -118,7 +118,28 @@ bool inaccurate_control_1D(float pos_sp, float pos, float &vel_sp)
 	}
 	return is_arrived;
 }
+bool inaccurate_control_2D(const Vector3f& pos_sp, const Vector3f& pos, Vector2f& vel_sp)
+{
+	float speed = 0.1;
+	bool is_arrived;
+	Vector3f err3 = pos_sp - pos;
+	err3(2) = 0;
+	Vector2f err;
+	err(0) = err3(0);
+	err(1) = err3(1);
+	float dist = err.norm();
+	if(dist > 0.3){
+		Vector2f direction = err / dist;
+		vel_sp = direction * speed;
 
+		is_arrived = false;
+	//	ROS_INFO("\npos(%f, %f)\nsetpt(%f, %f)\nvelsp(%f, %f)\n",_pos(0),_pos(1),_pos_sp(0),_pos_sp(1),vel_sp(0),vel_sp(1));
+	}
+	else{
+		is_arrived = true;
+	}
+	return is_arrived;
+}
 bool inaccurate_control_3D(const Vector3f& pos_sp, const Vector3f& pos, Vector3f& vel_sp)
 {
 	float speed = 0.1;
@@ -192,30 +213,56 @@ int main(int argc, char **argv)
 	ros::Subscriber ctrl_sub = n.subscribe("ctrl", 1, ctrl_sp_callback);
 	ros::Subscriber drone_info_sub = n.subscribe("/ardrone/position_reset_info", 1, drone_info_Callback);
 	ros::Rate loop_rate(LOOP_RATE);
-	bool arrived;
+	bool arrived, alt_arrived;
 	flight_strategy::ctrlBack ctrlBack_msg;
 	while(ros::ok()){
 		if(ctrl.mode == 0){
-			for(int i = 0 ; i < 3 ; i++){//xyz axis
-				if(ctrl.pos_halt[i]){
-					output.vel_sp[i] = 0;
-					arrived = false;
-				}
-				else{
-					float vel_sp_1D;
-					arrived = inaccurate_control_1D(ctrl.pos_sp[i], raw_state.pos_f(i), vel_sp_1D);
-				//	ROS_INFO("SetPt: %f\nPos: %f\n", ctrl.pos_sp[i], raw_state.pos_f(i));
+			// for(int i = 0 ; i < 3 ; i++){//xyz axis
+			// 	if(ctrl.pos_halt[i]){
+			// 		output.vel_sp[i] = 0;
+			// 		arrived = false;
+			// 	}
+			// 	else{
+			// 		float vel_sp_1D;
+			// 		arrived = inaccurate_control_1D(ctrl.pos_sp[i], raw_state.pos_f(i), &vel_sp_1D);
+			// 	//	ROS_INFO("SetPt: %f\nPos: %f\n", ctrl.pos_sp[i], raw_state.pos_f(i));
 
-					if(arrived){
-					//	ROS_INFO("arrived");
-						output.vel_sp[i] = 0;
-						ctrlBack_msg.arrived[i] = 1;
-					}
-					else{
-						ctrlBack_msg.arrived[i] = 0;
-						output.vel_sp[i] = vel_sp_1D;
-					}
-				}
+			// 		if(arrived){
+			// 		//	ROS_INFO("arrived");
+			// 			output.vel_sp[i] = 0;
+			// 			ctrlBack_msg.arrived[i] = 1;
+			// 		}
+			// 		else{
+			// 			ctrlBack_msg.arrived[i] = 0;
+			// 			output.vel_sp[i] = vel_sp_1D;
+			// 		}
+			// 	}
+			// }
+			Vector2f vel_sp;
+			arrived = inaccurate_control_2D(ctrl.pos_sp, raw_state.pos_f, vel_sp);
+			if(arrived){
+				ctrlBack_msg.arrived[0] = 1;
+				ctrlBack_msg.arrived[1] = 1;
+				output.vel_sp(0) = 0;
+				output.vel_sp(1) = 0;
+			}
+			else{
+				ctrlBack_msg.arrived[0] = 0;
+				ctrlBack_msg.arrived[1] = 0;
+				output.vel_sp(0) = vel_sp(0);
+				output.vel_sp(1) = vel_sp(1);
+			}
+			//z
+			float vel_sp_1D;
+			alt_arrived = inaccurate_control_1D(ctrl.pos_sp(2), raw_state.pos_f(2), &vel_sp_1D);
+			if(alt_arrived){
+			//	ROS_INFO("arrived");
+				output.vel_sp(2) = 0;
+				ctrlBack_msg.arrived[2] = 1;
+			}
+			else{
+				ctrlBack_msg.arrived[2] = 0;
+				output.vel_sp(2) = vel_sp_1D;
 			}
 		}
 		else if(ctrl.mode == 1){
@@ -225,23 +272,23 @@ int main(int argc, char **argv)
 			if(arrived){
 				ctrlBack_msg.arrived[0] = 1;
 				ctrlBack_msg.arrived[1] = 1;
-				output.vel_sp[0] = 0;
-				output.vel_sp[1] = 0;
-				output.vel_sp[2] = 0;
+				output.vel_sp(0) = 0;
+				output.vel_sp(1) = 0;
+				output.vel_sp(2) = 0;
 			}
 			else{
 				ctrlBack_msg.arrived[0] = 0;
 				ctrlBack_msg.arrived[1] = 0;
-				output.vel_sp[0] = vel_sp(0);
-				output.vel_sp[1] = vel_sp(1);
-				output.vel_sp[2] = 0;
+				output.vel_sp(0) = vel_sp(0);
+				output.vel_sp(1) = vel_sp(1);
+				output.vel_sp(2) = 0;
 			}
 		}
 		ctrlBack_pub.publish(ctrlBack_msg);
 		geometry_msgs::Twist cmd;
-		cmd.linear.x = output.vel_sp[0];
-		cmd.linear.y = output.vel_sp[1];
-		cmd.linear.z = output.vel_sp[2];
+		cmd.linear.x = output.vel_sp(0);
+		cmd.linear.y = output.vel_sp(1);
+		cmd.linear.z = output.vel_sp(2);
 		cmd_pub.publish(cmd);
 		ros::spinOnce();
 		loop_rate.sleep();
